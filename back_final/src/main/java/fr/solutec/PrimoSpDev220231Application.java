7ecb0bb16c6518935653200b9d804df28115a423
Auriax123 <auriane.orsati3@gmail.com>
2023-03-01 15:51:21 +0100

    premier commit

---
+++ back_final/src/main/java/fr/solutec/PrimoSpDev220231Application.java
@@ -0,0 +1,13 @@
+package fr.solutec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimoSpDev220231Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimoSpDev220231Application.class, args);
+	}
+
+}
---
+++ back_final/src/test/java/fr/solutec/PrimoSpDev220231ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.solutec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimoSpDev220231ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
