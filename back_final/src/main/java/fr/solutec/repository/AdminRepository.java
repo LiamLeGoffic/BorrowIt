e909a1c6e011c07ed88f3c81252ed92bad534833
IKent77 <kent.iyinbor@hotmail.fr>
2023-03-02 16:30:22 +0100

    Merge branch 'master' of https://github.com/LiamLeGoffic/BorrowIt

--- back_final/src/main/java/fr/solutec/repository/AdminRepository.java
+++ back_final/src/main/java/fr/solutec/repository/AdminRepository.java
@@ -10,7 +10,7 @@ import fr.solutec.entities.Administrateur;
 public interface AdminRepository extends CrudRepository<Administrateur, Long> {
 	
 	@Query("SELECT admin FROM Administrateur admin WHERE admin.mail = ?1 AND admin.motDePasse = ?2")
-	public Optional<Administrateur> connexionAdmin(String mail, String motDePasse);
+	public Optional<Administrateur> connexionAdmin(String mail, String motDePasse); 
 	
 	
 	
