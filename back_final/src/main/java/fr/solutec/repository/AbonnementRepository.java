7e8ffcf9069e8c425e34aeb75ac1e237e824694b
LiamLeGoffic <liamlegoffic@yahoo.fr>
2023-03-02 14:14:22 +0100

    new package et fichier sur abonnement

---
+++ back_final/src/main/java/fr/solutec/repository/AbonnementRepository.java
@@ -0,0 +1,9 @@
+package fr.solutec.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import fr.solutec.entities.Abonnement;
+
+public interface AbonnementRepository extends CrudRepository<Abonnement, Long> {
+
+}
