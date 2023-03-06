f6729dbbdb99863b985a281ca7888b7ac9ae0f11
brunaze <brunodspinto98@gmail.com>
2023-03-06 17:01:13 +0100

    Test messagerie 1
    Merge branch 'master' of https://github.com/LiamLeGoffic/BorrowIt

---
+++ back_final/src/main/java/fr/solutec/messagerie/ChatController.java
@@ -0,0 +1,14 @@
+package fr.solutec.messagerie;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+	
+	@Autowired
+	private SimpMessagingTemplate messagingTemplate;
+	
+
+}
---
+++ back_final/src/main/java/fr/solutec/messagerie/MessageStatus.java
@@ -0,0 +1,5 @@
+package fr.solutec.messagerie;
+
+public enum MessageStatus {
+	RECEIVED, DELIVERED
+}
